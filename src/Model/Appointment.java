d49f5cfad1e1da7fc4bb2afeddcd7ab0e189fa5a
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-04-30 00:03:19 -0700

    updated appointment class

--- src/Model/Appointment.java
+++ src/Model/Appointment.java
@@ -1,9 +1,20 @@
 package Model;
 
+import Main.Main;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.Date;
 import java.sql.Timestamp;
 
 public class Appointment {
+    private ObservableList allAppointments;
     private int apptId;
     private String title;
     private String description;
@@ -19,7 +30,8 @@ public class Appointment {
     private int userId;
     private int contactId;
 
-    public Appointment(int apptId, String title, String description, String location, String type, Date startDate, Date endDate, Date createdDate, String createdBy, Timestamp lastUpdate, String lastUpdatedBy, int customerId, int userId, int contactId) {
+    public Appointment(ObservableList allAppointments, int apptId, String title, String description, String location, String type, Date startDate, Date endDate, Date createdDate, String createdBy, Timestamp lastUpdate, String lastUpdatedBy, int customerId, int userId, int contactId) {
+        this.allAppointments = allAppointments;
         this.apptId = apptId;
         this.title = title;
         this.description = description;
@@ -34,6 +46,14 @@ public class Appointment {
         this.customerId = customerId;
         this.userId = userId;
         this.contactId = contactId;
+    }
+
+    public static void apptsMenu(Event event) throws IOException {
+        Parent root = FXMLLoader.load(Main.class.getResource("/View/CreateScheduleForm.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
 
