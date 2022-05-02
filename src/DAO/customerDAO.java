f66535d5f2e7164579aa2ab80113b4a45b19e1b7
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-05-01 23:32:56 -0700

    added more CRUD functions

--- src/DAO/customerDAO.java
+++ src/DAO/customerDAO.java
@@ -1,4 +1,58 @@
 package DAO;
 
-public class customerDAO {
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static DAO.JDBC_Connector.connection;
+
+public abstract class customerDAO {
+
+    /**
+     * Creates customer records.
+     */
+//    public static int create() throws SQLException {
+//        String sqlStatement = "INSERT INTO customers(Customer_ID, Customer_Name, Address, Postal_Code, Phone, Create_Date, Created_By, Last_Update, Last_Updated_By, Division_ID) VALUES(?,?,?,?,?,?,?,?,?,?)";
+//        PreparedStatement statement = connection.prepareStatement(sqlStatement);
+//    }
+
+
+    public static void selectCustomer() throws SQLException {
+        String sqlStatement = "SELECT * FROM customers";
+        PreparedStatement statement = connection.prepareStatement(sqlStatement);
+        ResultSet results = statement.executeQuery();
+
+        while (results.next()) {
+            int userId = results.getInt("User_ID");
+            String userName = results.getString("User_Name");
+        }
+    }
+
+    public static boolean getLogin(String username, String password) throws SQLException {
+        String sqlStatement = "SELECT * FROM users WHERE User_ID = ? AND Password = ?";
+        PreparedStatement statement = connection.prepareStatement(sqlStatement);
+        statement.setString(1, username);
+        statement.setString(2, password);
+        ResultSet results = statement.executeQuery();
+
+        try {
+            if(results.next()) {
+                int userId = results.getInt("User_ID");
+
+                String pwd = results.getString("Password");
+                System.out.println(userId + " " + pwd);
+                return true;
+            }
+            else if(!results.next()){
+                return false;
+            }
+            else {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -1,4 +1,110 @@
 package Model;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
 public class Customer {
+    private int customerID;
+    private String customerName;
+    private String address;
+    private String postalCode;
+    private String phoneNumber;
+    private Date createDate;
+    private String createdBy;
+    private Timestamp lastUpdate;
+    private String lastUpdatedBy;
+    private int divID;
+
+    public Customer(int customerID, String customerName, String address, String postalCode, String phoneNumber, Date createDate, String createdBy, Timestamp lastUpdate, String lastUpdatedBy, int divID){
+        this.customerID = customerID;
+        this.customerName = customerName;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.phoneNumber = phoneNumber;
+        this.createDate = createDate;
+        this.createdBy = createdBy;
+        this.lastUpdate = lastUpdate;
+        this.lastUpdatedBy = lastUpdatedBy;
+        this.divID = divID;
+    }
+
+    public int getCustomerID(){
+        return customerID;
+    }
+
+    public String getCustomerName(){
+        return customerName;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public String getPostalCode(){
+        return postalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy;
+    }
+
+    public int getDivID(){
+        return divID;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setDivID(int divID) {
+        this.divID = divID;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy = lastUpdatedBy;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
 }
