223742839ca7d4bb0135ddcd3a3e84173e69891a
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-05-05 00:37:20 -0700

    edited Appointment class

--- src/DAO/appointmentDAO.java
+++ src/DAO/appointmentDAO.java
@@ -1,7 +1,7 @@
 package DAO;
 
+import Controller.LoginController;
 import Model.Appointment;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.sql.*;
@@ -15,33 +15,91 @@ import static DAO.JDBC_Connector.connection;
 
 public abstract class appointmentDAO {
 
-    static ObservableList<Appointment> appointments = FXCollections.observableArrayList();
-
-    public static ObservableList<Appointment> selectAppointments() throws SQLException {
+    public static ObservableList<Appointment> getAllAppts() throws SQLException {
         String sqlStatement = "SELECT * FROM appointments";
+
         PreparedStatement statement = connection.prepareStatement(sqlStatement);
         ResultSet results = statement.executeQuery();
 
+        Appointment appointment;
         while (results.next()) {
-            int apptId = results.getInt("Appointment_ID");
-            String title = results.getString("Title");
-            String description = results.getString("Description");
-            String location = results.getString("Location");
-            String Type = results.getString("Type");
-            Date start_date = results.getDate("Start");
-            Date end_date = results.getDate("End");
-            Date create_date = results.getDate("Create_Date");
-            String created_by = results.getString("Created_By");
-            Timestamp last_update = results.getTimestamp("Last_Update");
-            String last_updated_by = results.getString("Last_Updated_By");
-            int customer_id = results.getInt("Customer_ID");
-            int user_id = results.getInt("User_ID");
-            int contact_id = results.getInt("Contact_ID");
-
-            Appointment appointment = new Appointment(apptId, title, description, location, Type, start_date, end_date, create_date, created_by, last_update, last_updated_by, customer_id, user_id, contact_id);
+            appointment = new Appointment();
+            appointment.setApptId(results.getInt("Appointment_ID"));
+            appointment.setTitle(results.getString("Title"));
+            appointment.setDescription(results.getString("Description"));
+            appointment.setLocation(results.getString("Location"));
+            appointment.setType(results.getString("Type"));
+            appointment.setStartDate(results.getDate("Start"));
+            appointment.setEndDate(results.getDate("End"));
+            appointment.setCreatedDate(results.getDate("Create_Date"));
+            appointment.setCreatedBy(results.getString("Created_By"));
+            appointment.setLastUpdate(results.getTimestamp("Last_Update"));
+            appointment.setLastUpdatedBy(results.getString("Last_Updated_By"));
+            appointment.setCustomerId(results.getInt("Customer_ID"));
+            appointment.setUserId(results.getInt("User_ID"));
+            appointment.setContactId(results.getInt("Contact_ID"));
             Appointment.addAppointment(appointment);
-
         }
         return Appointment.getAllAppointments();
     }
+
+    public static ObservableList<Appointment> getUserAppts() throws SQLException {
+        String sqlStatement = "SELECT * FROM appointments WHERE User_ID = ?";
+
+        int userId = LoginController.getUserID();
+
+        PreparedStatement statement = connection.prepareStatement(sqlStatement);
+        statement.setInt(1, userId);
+        ResultSet results = statement.executeQuery();
+
+        Appointment appointment;
+        while (results.next()) {
+            appointment = new Appointment();
+            appointment.setApptId(results.getInt("Appointment_ID"));
+            appointment.setTitle(results.getString("Title"));
+            appointment.setDescription(results.getString("Description"));
+            appointment.setLocation(results.getString("Location"));
+            appointment.setType(results.getString("Type"));
+            appointment.setStartDate(results.getDate("Start"));
+            appointment.setEndDate(results.getDate("End"));
+            appointment.setCreatedDate(results.getDate("Create_Date"));
+            appointment.setCreatedBy(results.getString("Created_By"));
+            appointment.setLastUpdate(results.getTimestamp("Last_Update"));
+            appointment.setLastUpdatedBy(results.getString("Last_Updated_By"));
+            appointment.setCustomerId(results.getInt("Customer_ID"));
+            appointment.setUserId(results.getInt("User_ID"));
+            appointment.setContactId(results.getInt("Contact_ID"));
+            Appointment.addAppointment(appointment);
+        }
+        return Appointment.getAllAppointments();
+    }
+
+    public static int createAppt(String Title, String Description, String Location, String Type, Date Start, Date End, Date create_date, String created_by, Timestamp last_update, String last_updated_by, Integer customerId, Integer userId, Integer contactId) throws SQLException {
+        String sqlStatement = "INSERT INTO Appointments(Title, Description, Location, Type, Start, End, Create_Date, Created_By, Last_Update, Last_Updated_By, Customer_ID, User_ID, Contact_ID) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+        PreparedStatement statement = connection.prepareStatement(sqlStatement);
+        statement.setString(1, Title);
+        statement.setString(2, Description);
+        statement.setString(3, Location);
+        statement.setString(4, Type);
+        statement.setDate(5, Start);
+        statement.setDate(6, End);
+        statement.setDate(7, create_date);
+        statement.setString(8, created_by);
+        statement.setTimestamp(9, last_update);
+        statement.setString(10, last_updated_by);
+        statement.setInt(11, customerId);
+        statement.setInt(12, userId);
+        statement.setInt(13, contactId);
+
+        return statement.executeUpdate();
+    }
+
+    public static int deleteAppt(Integer apptID) throws SQLException {
+        String sqlStatement = "DELETE FROM Appointments WHERE Appointment_ID = ?";
+        PreparedStatement statement = connection.prepareStatement(sqlStatement);
+        statement.setInt(1, apptID);
+        return statement.executeUpdate();
+    }
+
 }
--- src/Model/Appointment.java
+++ src/Model/Appointment.java
@@ -1,6 +1,6 @@
 package Model;
 
-import DAO.appointmentDAO;
+import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -9,43 +9,46 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.sql.Date;
 import java.sql.Timestamp;
 
 public class Appointment {
     private static final ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
-    private String description;
-    private String location;
-    private String type;
-    private Date startDate;
-    private Date endDate;
-    private Date createdDate;
-    private String createdBy;
-    private Timestamp lastUpdate;
-    private String lastUpdatedBy;
-    private int customerId;
-    private int userId;
-    private int contactId;
+    private final SimpleIntegerProperty apptId;
+    private final SimpleStringProperty title;
+    private final SimpleStringProperty description;
+    private final SimpleStringProperty location;
+    private final SimpleStringProperty type;
+    private final SimpleObjectProperty<Date> startDate;
+    private final SimpleObjectProperty<Date> endDate;
+    private final SimpleObjectProperty<Date> createdDate;
+    private final SimpleStringProperty createdBy;
+    private final SimpleObjectProperty<Timestamp> lastUpdate;
+    private final SimpleStringProperty lastUpdatedBy;
+    private final SimpleIntegerProperty customerId;
+    private final SimpleIntegerProperty userId;
+    private final SimpleIntegerProperty contactId;
 
-    public Appointment(int apptId, String title, String description, String location, String type, Date startDate, Date endDate, Date createdDate, String createdBy, Timestamp lastUpdate, String lastUpdatedBy, int customerId, int userId, int contactId) {
-        this.description = description;
-        this.location = location;
-        this.type = type;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.createdDate = createdDate;
-        this.createdBy = createdBy;
-        this.lastUpdate = lastUpdate;
-        this.lastUpdatedBy = lastUpdatedBy;
-        this.customerId = customerId;
-        this.userId = userId;
-        this.contactId = contactId;
+    public Appointment() {
+        this.apptId = new SimpleIntegerProperty();
+        this.title = new SimpleStringProperty();
+        this.description = new SimpleStringProperty();
+        this.location = new SimpleStringProperty();
+        this.type = new SimpleStringProperty();
+        this.startDate = new SimpleObjectProperty<>();
+        this.endDate = new SimpleObjectProperty<>();
+        this.createdDate = new SimpleObjectProperty<>();
+        this.createdBy = new SimpleStringProperty();
+        this.lastUpdate = new SimpleObjectProperty<>();
+        this.lastUpdatedBy = new SimpleStringProperty();
+        this.customerId = new SimpleIntegerProperty();
+        this.userId = new SimpleIntegerProperty();
+        this.contactId = new SimpleIntegerProperty();
     }
 
     public static void mainMenu(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Main.Main.class.getResource("/View/createAppointment.fxml"));
+        Parent root = FXMLLoader.load(Main.Main.class.getResource("/View/appointmentForm.fxml"));
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
@@ -60,5 +63,115 @@ public class Appointment {
         allAppointments.add(newAppointment);
     }
 
+    public int getApptId() {
+        return apptId.get();
+    }
+
+    public void setApptId(int apptId) {
+        this.apptId.set(apptId);
+    }
+
+    public String getTitle() {
+        return title.get();
+    }
+
+    public void setTitle(String title) {
+        this.title.set(title);
+    }
+
+    public String getDescription() {
+        return description.get();
+    }
+
+    public void setDescription(String description) {
+        this.description.set(description);
+    }
+
+    public String getLocation() {
+        return location.get();
+    }
+
+    public void setLocation(String location) {
+        this.location.set(location);
+    }
+
+    public String getType() {
+        return type.get();
+    }
+
+    public void setType(String type) {
+        this.type.set(type);
+    }
+
+    public Date getStartDate() {
+        return startDate.get();
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate.set(startDate);
+    }
+
+    public Date getEndDate() {
+        return endDate.get();
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate.set(endDate);
+    }
+
+    public Date getCreatedDate() {
+        return createdDate.get();
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate.set(createdDate);
+    }
+
+    public String getCreatedBy() {
+        return createdBy.get();
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy.set(createdBy);
+    }
+
+    public String getLastUpdatedBy() {
+        return lastUpdatedBy.get();
+    }
+
+    public void setLastUpdatedBy(String lastUpdatedBy) {
+        this.lastUpdatedBy.set(lastUpdatedBy);
+    }
+
+    public int getCustomerId() {
+        return customerId.get();
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId.set(customerId);
+    }
+
+    public int getContactId() {
+        return contactId.get();
+    }
+
+    public void setContactId(int contactId) {
+        this.contactId.set(contactId);
+    }
+
+    public int getUserId() {
+        return userId.get();
+    }
+
+    public void setUserId(int userId) {
+        this.userId.set(userId);
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate.get();
+    }
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate.set(lastUpdate);
+    }
 }
 
