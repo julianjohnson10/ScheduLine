124a80fc810d779727763143b3104d161ddd8162
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-05-07 10:25:07 -0700

    edited alertBox.java

--- src/Utilities/alertBox.java
+++ src/Utilities/alertBox.java
@@ -3,11 +3,16 @@ package Utilities;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
-
 import java.util.Optional;
 
 public class alertBox {
-
+    /**
+     * Custom Alert Box.
+     * @param title Title of the alert box.
+     * @param message Message to the user.
+     * @param alertType Alertbox type.
+     * @return The alertBox.
+     */
     public static Optional<ButtonType> raiseAlert(String title, String message, AlertType alertType){
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
