7b880eaa8d5ca5e7a04bc345ed4bef495947bcc6
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-05-12 01:45:06 -0700

    updated sql to get all appts

--- src/DAO/appointmentDAO.java
+++ src/DAO/appointmentDAO.java
@@ -22,10 +22,10 @@ public abstract class appointmentDAO {
      * ERROR: SQLException: Column 'Created_Date' not found. Fixed by changing to 'Create_Date'
      */
     public static ObservableList<Appointment> getAllAppts() throws SQLException {
-        String sqlStatement = "SELECT * FROM appointments";
-        ObservableList<Appointment> appointmentList = FXCollections.observableArrayList();
+        String sqlStatement = "SELECT * FROM appointments LEFT JOIN contacts ON contacts.Contact_ID = appointments.Contact_ID;";
         PreparedStatement statement = connection.prepareStatement(sqlStatement);
         ResultSet results = statement.executeQuery();
+        ObservableList<Appointment> apptsList = FXCollections.observableArrayList();
 
         while (results.next()) {
             Appointment appointment = new Appointment();
@@ -35,45 +35,47 @@ public abstract class appointmentDAO {
             appointment.setTitle(results.getString("Title"));
             appointment.setDescription(results.getString("Description"));
             appointment.setLocation(results.getString("Location"));
-
-
-
-            appointment.setContactName(contactDAO.getApptContacts(results.getInt("Contact_ID")));
+            appointment.setContactName(results.getString("Contact_Name"));
             appointment.setType(results.getString("Type"));
 
-            LocalDateTime start_dateL = (LocalDateTime) results.getObject("Start");
-            String startDate = start_dateL.format(format);
+            ZonedDateTime start_dateZ = ((LocalDateTime) results.getObject("Start")).atZone(ZoneId.systemDefault());
+            String startDate = start_dateZ.format(format);
             appointment.setStartDate(startDate);
 
-            LocalDateTime end_dateL = (LocalDateTime) results.getObject("End");
-            String endDate = end_dateL.format(format);
-            appointment.setEndDate(endDate);
+            LocalDateTime endDate = (LocalDateTime) results.getObject("End");
+
+//            appointment.setEndDate(endDate);
+            System.out.println(results.getString("End"));
+//            System.out.println(end_dateZ);
+            System.out.println(endDate);
 
             appointment.setCustomerId(results.getInt("Customer_ID"));
             appointment.setUserId(results.getInt("User_ID"));
-            appointmentList.add(appointment);
-            System.out.println(appointment.getContactName());
+            appointment.setContactId(results.getInt("Contact_ID"));
+
+            Appointment.addAppointment(appointment);
+
+            apptsList.add(appointment);
         }
-        return appointmentList;
+        return apptsList;
     }
 
-    public static int createAppt(String Title, String Description, String Location, String Type, Date Start, Date End, Date create_date, String created_by, Timestamp last_update, String last_updated_by, Integer customerId, Integer userId, Integer contactId) throws SQLException {
-        String sqlStatement = "INSERT INTO Appointments(Title, Description, Location, Type, Start, End, Create_Date, Created_By, Last_Update, Last_Updated_By, Customer_ID, User_ID, Contact_ID) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    public static int createAppt(String Title, String Description, String Location, String contact, String Type, LocalDate apptDate, LocalTime startTime, LocalTime endTime, Integer customerId, Integer userId) throws SQLException {
+        String sqlStatement = "INSERT INTO Appointments(Title, Description, Location, Type, Start, End, Create_Date, Created_By, Last_Update, Last_Updated_By, Customer_ID, User_ID) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
 
         PreparedStatement statement = connection.prepareStatement(sqlStatement);
         statement.setString(1, Title);
         statement.setString(2, Description);
         statement.setString(3, Location);
         statement.setString(4, Type);
-        statement.setDate(5, Start);
-        statement.setDate(6, End);
-        statement.setDate(7, create_date);
-        statement.setString(8, created_by);
-        statement.setTimestamp(9, last_update);
-        statement.setString(10, last_updated_by);
+        statement.setObject(5, LocalDate.now());
+        statement.setObject(6, LocalDate.now());
+        statement.setObject(7, LocalDateTime.now());
+        statement.setString(8, userDAO.getUserInfo().getUserName());
+        statement.setObject(9, LocalDateTime.now());
+        statement.setString(10, userDAO.getUserInfo().getUserName());
         statement.setInt(11, customerId);
         statement.setInt(12, userId);
-        statement.setInt(13, contactId);
 
         return statement.executeUpdate();
     }
@@ -100,11 +102,11 @@ public abstract class appointmentDAO {
     }
 
 
-    public static int deleteAppt(Integer apptID) throws SQLException {
+    public static void deleteAppt(Integer apptID) throws SQLException {
         String sqlStatement = "DELETE FROM Appointments WHERE Appointment_ID = ?";
         PreparedStatement statement = connection.prepareStatement(sqlStatement);
         statement.setInt(1, apptID);
-        return statement.executeUpdate();
+        statement.executeUpdate();
     }
 
     public static boolean checkCustomer(Integer customerID) throws SQLException {
@@ -126,4 +128,16 @@ public abstract class appointmentDAO {
         }
         return false;
     }
+
+    public static int getAppointmentID() throws SQLException {
+        String sqlStatement = "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = \"client_schedule\" AND TABLE_NAME = \"appointments\"";
+        PreparedStatement statement = connection.prepareStatement(sqlStatement);
+        ResultSet result = statement.executeQuery();
+        int apptID = 0;
+        while (result.next()) {
+            apptID = result.getInt("AUTO_INCREMENT");
+        }
+        return apptID;
+    }
+
 }
