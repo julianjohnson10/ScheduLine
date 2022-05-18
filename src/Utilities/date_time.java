8c06db168b1a0aea60595d82c704ac60963572d9
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-05-17 23:05:48 -0700

    built timestamp method

--- src/Utilities/date_time.java
+++ src/Utilities/date_time.java
@@ -2,22 +2,24 @@ package Utilities;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 public class date_time {
 
-    public static String timestamp(Timestamp timestamp) {
-        SimpleDateFormat date = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-        return date.format(timestamp);
+    public static Timestamp setTimestamp() {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault());
+        ZonedDateTime zonedDateTime1 = zonedDateTime.withZoneSameInstant(ZoneId.of("UTC"));
+        LocalDateTime localDateTime1 = zonedDateTime1.toLocalDateTime();
+        return Timestamp.valueOf(localDateTime1);
     }
 
     public static ObservableList<String> startList = FXCollections.observableArrayList();
 
     public static void setStartList(){
+
         String pattern = "hh:mma";
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
         ZoneId zone = ZoneId.of("US/Eastern");
@@ -35,24 +37,16 @@ public class date_time {
     }
 
     public static void printDates(){
-        String pattern = "HH:mm";
+
+        String pattern = "hh:mma";
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
-        ZoneId zone = ZoneId.of("US/Eastern");
-        ZoneId defaultZone = ZoneId.systemDefault();
-        LocalDate localDate = LocalDate.now();
+        LocalDateTime localDate = LocalDateTime.now();
+        LocalTime localTime = LocalTime.of(localDate.getHour(),localDate.getMinute());
+        System.out.println(localTime.format(dateTimeFormatter));
 
 
-        for(int i = 8; i < 22; i++){
-            for(int n = 0; n<=30; n+=30){
-                LocalTime localTime = LocalTime.of(i,n);
-
-                ZonedDateTime zonedDateTime = ZonedDateTime.of(localDate,localTime,zone);
-                ZonedDateTime zonedDateTime1 = zonedDateTime.withZoneSameInstant(ZoneId.of(String.valueOf(defaultZone)));
-
-                System.out.println(zonedDateTime1);
-            }
-        }
     }
+
 
 
 //
