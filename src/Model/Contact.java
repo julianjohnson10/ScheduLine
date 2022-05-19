693eb5edfab3ff1cfeb4d038906324f4fbb77641
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-05-19 04:30:28 -0700

    removed unused model objects

--- src/Model/Contact.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public class Contact {
-    private int contactID;
-    private String contactName;
-
-}
--- src/Model/Division.java
+++
@@ -1,6 +0,0 @@
-package Model;
-
-public class Division {
-    private int divId;
-
-}
