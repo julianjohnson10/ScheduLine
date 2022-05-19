9ed9f61c74df7882dc117351ae2f64a8218e0d9c
julian.johnson.10 <julian.johnson.10@outlook.com>
2022-05-19 04:29:07 -0700

    added README.txt Updated language properties

---
+++ README.txt
@@ -0,0 +1,14 @@
+Title: Scheduling Application
+Purpose: To create customers and to schedule customer appointments.
+Author: Julian Johnson
+Contact Info: jjo2644@wgu.edu
+Version: v1.0
+Date: 18-May-2022
+IDE: IntelliJ IDEA 2021.3.3 (Community Edition)
+JDK: Java SE 17.0.2
+JavaFX Version: JavaFX-SDK-17.0.2
+
+Directions:
+1. Download JavaFX-SDK-17.0.2 and
+
+MySQL Driver Version: mysql-connector-java-8.0.27
--- src/Utilities/activityLogger.java
+++ src/Utilities/activityLogger.java
@@ -4,21 +4,33 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE;
 
-
+/**
+ * Class for logging Login activity.
+ */
 public class activityLogger {
 
+    /**
+     * Each attempt is logged and appended to a file.
+     * @param user the user name.
+     * @param login If login was true or false.
+     * @throws IOException exception handler.
+     */
     public static void logActivity(String user, boolean login) throws IOException {
+
         Path filePath = Path.of("login_activity.txt");
         if(login){
-            Files.writeString(filePath, "User: " +user+"\nLogin Status: Successful Login\nTimeStamp: " + Timestamp.valueOf(LocalDateTime.now()) + "\n\n", CREATE, APPEND);
+            Files.writeString(filePath, "User: " +user+"\nLogin Status: Successful Login\nTimeStamp: " + date_time.setTimestamp() + "\n\n", CREATE, APPEND);
         }
         else{
-            Files.writeString(filePath, "User: " +user+ "\nLogin Status: Failed Login Attempt\nTimeStamp: " + Timestamp.valueOf(LocalDateTime.now()) + "\n\n", CREATE, APPEND);
+            Files.writeString(filePath, "User: " +user+ "\nLogin Status: Failed Login Attempt\nTimeStamp: " + date_time.setTimestamp() + "\n\n", CREATE, APPEND);
         }
     }
 }
